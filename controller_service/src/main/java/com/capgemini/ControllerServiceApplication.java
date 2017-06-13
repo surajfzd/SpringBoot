325886530432863151c90695b57b6988b06e47aa
Degaonkar <sanjay.degaonkar@capgemini.com>
2017-06-13 16:42:11 +0530

    writer controller and service

---
+++ controller_service/src/main/java/com/capgemini/ControllerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerServiceApplication.class, args);
+	}
+}
